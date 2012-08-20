5bf3b7bd2ac038fc14bdaa3d4f9c1940a1735a8f
blueboy <djwebb@blueyonder.co.uk>
2012-08-20 15:55:29 +0100

    Fix to prevent merge conflicts, when sql changes occur &  provide a more relevant db error message, should the versions  not match.

--- src/shared/revision_sql.h
+++ src/shared/revision_sql.h
@@ -1,7 +1,6 @@
 #ifndef __REVISION_SQL_H__
 #define __REVISION_SQL_H__
  #define REVISION_DB_CHARACTERS "required_11785_02_characters_instance"
- #define REVISION_DB_PLAYERBOTAI "required_5_playerbotai_auto_follow"
  #define REVISION_DB_MANGOS "required_12148_02_mangos_mangos_string"
  #define REVISION_DB_REALMD "required_12112_01_realmd_account_access"
 #endif // __REVISION_SQL_H__
