e3ae3c5f5f20c2f415939ac6bbd672021deedf83
kennumen <kvandaele@hotmail.com>
2012-08-25 17:14:48 +0200

    [tweak] Merge primorder and secorder into combat_order for db as well as code. Add safety check so only a single primary order can be active. Streamline various combat order code. Merge resist types into combat_order.

--- src/game/playerbot/PlayerbotShamanAI.cpp
+++ src/game/playerbot/PlayerbotShamanAI.cpp
@@ -360,7 +360,7 @@ void PlayerbotShamanAI::DropTotems()
     // Fire Totems
     if ((fire == NULL) || (m_bot->GetDistance(fire) > 30))
     {
-        if (m_ai->GetResistType() == PlayerbotAI::SCHOOL_FROST && FROST_RESISTANCE_TOTEM > 0 && m_ai->CastSpell(FROST_RESISTANCE_TOTEM))
+        if (m_ai->GetCombatOrder() & PlayerbotAI::ORDERS_RESIST_FROST && FROST_RESISTANCE_TOTEM > 0 && m_ai->CastSpell(FROST_RESISTANCE_TOTEM))
             ;
         else if (spec == SHAMAN_SPEC_ELEMENTAL && TOTEM_OF_WRATH > 0 && m_ai->CastSpell(TOTEM_OF_WRATH))
             ;
@@ -372,7 +372,7 @@ void PlayerbotShamanAI::DropTotems()
     // Air totems
     if ((air == NULL) || (m_bot->GetDistance(air) > 30))
     {
-        if (m_ai->GetResistType() == PlayerbotAI::SCHOOL_NATURE && NATURE_RESISTANCE_TOTEM > 0 && m_ai->CastSpell(NATURE_RESISTANCE_TOTEM))
+        if (m_ai->GetCombatOrder() == PlayerbotAI::ORDERS_RESIST_NATURE && NATURE_RESISTANCE_TOTEM > 0 && m_ai->CastSpell(NATURE_RESISTANCE_TOTEM))
             ;
         else if (spec == SHAMAN_SPEC_ENHANCEMENT)
         {
@@ -389,7 +389,7 @@ void PlayerbotShamanAI::DropTotems()
     // Water Totems
     if ((water == NULL) || (m_bot->GetDistance(water) > 30))
     {
-        if (m_ai->GetResistType() == PlayerbotAI::SCHOOL_FIRE && FIRE_RESISTANCE_TOTEM > 0 && m_ai->CastSpell(FIRE_RESISTANCE_TOTEM))
+        if (m_ai->GetCombatOrder() == PlayerbotAI::ORDERS_RESIST_FIRE && FIRE_RESISTANCE_TOTEM > 0 && m_ai->CastSpell(FIRE_RESISTANCE_TOTEM))
             ;
         else if (MANA_SPRING_TOTEM > 0 && m_ai->CastSpell(MANA_SPRING_TOTEM))
             ;
